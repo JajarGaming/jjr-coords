95a2c9901703e005934f0024a18dd1ce0bfd64f8
Casper <71441748+JajarGaming@users.noreply.github.com>
2021-12-08 16:53:25 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,5 +1,5 @@
 RegisterCommand('coords', function(source, args, rawCommand)
-	local coords = GetEntityCoords(PlayerPedId())
+  local coords = GetEntityCoords(PlayerPedId())
   local heading = GetEntityHeading(PlayerPedId())
   TriggerEvent('chat:addMessage', {color = { 255, 0, 0}, multiline = true, args = {"System", coords.x..", "..coords.y..", "..coords.z.." Heading: "..heading} })
 end)
